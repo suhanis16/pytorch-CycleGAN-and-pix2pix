c0c4a712667060a0070d7c2f745feaec7d197921
malhaar2002 <aroramalhaar@gmail.com>
2024-05-08 19:58:49 +0530

    another try

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
-        for i, data in enumerate(dataloader):  # inner loop within one epoch
+        for data in dataloader:  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
